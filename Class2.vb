063f6a70c2bccfdf35c673052bde96bbcc72d313
Dungo <riezenlyledungo@gmail.com>
2025-11-27 10:43:50 +0800

    Update the class rounded button

--- Class2.vb
+++ Class2.vb
@@ -10,15 +10,28 @@ Public Class RoundedButton
         Me.FlatAppearance.BorderSize = 0
         Me.BackColor = Color.LightBlue
         Me.ForeColor = Color.Black
+        ' Optional: minimum size to avoid zero-size in Designer
+        Me.MinimumSize = New Size(50, 25)
     End Sub
 
     Protected Overrides Sub OnPaint(ByVal pevent As PaintEventArgs)
+        ' Designer-safe rendering
+        If Me.DesignMode Then
+            Using brush As New SolidBrush(Me.BackColor)
+                pevent.Graphics.FillRectangle(brush, 0, 0, Math.Max(50, Me.Width), Math.Max(25, Me.Height))
+            End Using
+            TextRenderer.DrawText(pevent.Graphics, Me.Text, Me.Font, New Rectangle(0, 0, Math.Max(50, Me.Width), Math.Max(25, Me.Height)), Me.ForeColor, TextFormatFlags.HorizontalCenter Or TextFormatFlags.VerticalCenter)
+            Return
+        End If
+
+        ' Runtime: original rounded painting
         Dim graphics As Graphics = pevent.Graphics
         graphics.SmoothingMode = SmoothingMode.AntiAlias
 
         Dim rect As Rectangle = New Rectangle(0, 0, Me.Width, Me.Height)
         Dim path As GraphicsPath = New GraphicsPath()
         Dim radius As Integer = 33
+
 
         path.AddArc(rect.X, rect.Y, radius, radius, 180, 90)
         path.AddArc(rect.Right - radius, rect.Y, radius, radius, 270, 90)
@@ -32,11 +45,6 @@ Public Class RoundedButton
         Using brush As New SolidBrush(Me.BackColor)
             graphics.FillPath(brush, path)
         End Using
-
-        ' Draw border
-        ' Using pen As New Pen(Color.Black, 2)
-        'graphics.DrawPath(pen, path)
-        'End Using
 
         ' Draw text
         TextRenderer.DrawText(graphics, Me.Text, Me.Font, rect, Me.ForeColor, TextFormatFlags.HorizontalCenter Or TextFormatFlags.VerticalCenter)
