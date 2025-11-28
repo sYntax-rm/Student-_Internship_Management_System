f1107d77e678179a7fb1cfebe413985a5fa78282
Dungo <riezenlyledungo@gmail.com>
2025-11-28 21:24:13 +0800

    Made an InternGeneratedID function

--- IDGenerator.vb
+++ IDGenerator.vb
@@ -98,6 +98,36 @@ Module IDGenerator
 
     End Function
 
+    Public Function GenerateInternID() As String
+        Dim currentMax As Integer = 0
+
+        Try
+            Using con As MySqlConnection = GetConnection()
+                Using cmd As New MySqlCommand("SELECT MAX(CAST(SUBSTRING(internship_id, 2) AS UNSIGNED)) FROM internship;", con)
+                    con.Open()
+
+                    Dim result = cmd.ExecuteScalar()
+                    If IsDBNull(result) Then
+                        currentMax = 0
+                    Else
+                        currentMax = Convert.ToInt32(result)
+                    End If
+
+                    con.Close()
+                End Using
+            End Using
+
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Database", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+
+        ' Increment by 1 and format with leading zeros
+        currentMax += 1
+
+        Return "I" & currentMax.ToString("D3")  ' D3 → 3 digits, e.g. I001
+    End Function
+
 
 
 End Module
