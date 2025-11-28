c0bb5841ddede0004d0698a0f6344a9f1008adf6
Dungo <riezenlyledungo@gmail.com>
2025-11-28 21:21:50 +0800

    Fix error missing function

--- IDGenerator.vb
+++ IDGenerator.vb
@@ -33,6 +33,40 @@ Module IDGenerator
         Return "F" & currentMax.ToString("D3")  ' D3 → 3 digits, e.g. F001
     End Function
 
+
+
+    Function GenerateDepartmentID(department As String) As String
+
+        Dim depID As String = ""
+
+        'College of Computer Studies D001
+        'College of Engineering DOO2
+        'College of Nursing D003
+        'College of Hospitality Management D004
+        'College of Education D005
+        'College of Business And Accountancy DOO6
+        'College of Arts And Sciences D007
+
+        If department = "College of Arts And Sciences" Then
+            depID = "D007"
+        ElseIf department = "College of Business And Accountancy" Then
+            depID = "D006"
+        ElseIf department = "College of Education" Then
+            depID = "D005"
+        ElseIf department = "College of Engineering" Then
+            depID = "D002"
+        ElseIf department = "College of Computer Studies" Then
+            depID = "D001"
+        ElseIf department = "College of Hospitality Management" Then
+            depID = "D004"
+        ElseIf department = "College of Nursing" Then
+            depID = "D003"
+        End If
+
+        Return depID
+    End Function
+
+
     Public Function GenerateStudentID() As String
         Dim currentMax As Integer = 0
 
